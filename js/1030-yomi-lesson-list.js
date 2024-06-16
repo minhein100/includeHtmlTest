fa47b165f5633290284c2b9c4b268dae09da3263
minhein100 <minhein100@gmail.com>
2024-06-17 08:40:00 +0900

    Add files via upload

---
+++ js/1030-yomi-lesson-list.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("#lessonListArea2")
+    .append('<div id="lessonListButton2"><a href=\"/html/1030yomi1.html\" style="text-decoration:none;">０１</a></div>');
+    $("#lessonListArea2")
+    .append('<div id="lessonListButton2"><a href=\"/html/1030yomi2.html\" style="text-decoration:none;">０２</a></div>');
+    $("#lessonListArea2")
+    .append('<div id="lessonListButton2"><a href=\"/html/1030yomi3.html\" style="text-decoration:none;">０３</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">０４</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">０５</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">０６</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">０７</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">０８</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">０９</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">１０</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">１１</a></div>');
+    // $("#lessonListArea2")
+    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">１２</a></div>');
+});
